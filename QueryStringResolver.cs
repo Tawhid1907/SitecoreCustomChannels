594bb0a0a4b946e57e3ab32f9c8e3b2c52144163
Syed Tawhidur Rahman <tawhid.info@gmail.com>
2019-12-01 23:17:35 +1100

    Create QueryStringResolver.cs

---
+++ QueryStringResolver.cs
@@ -0,0 +1,33 @@
+public class QueryStringChannelResolver : DetermineChannelProcessorBase
+{
+public override void Process(DetermineChannelProcessorArgs args)
+{
+try
+{
+Assert.ArgumentNotNull((object) args, nameof(args));
+if (Sitecore.Context.Request?.QueryString?.QueryStrings != null &&
+string.IsNullOrWhiteSpace(Sitecore.Context.Request?.QueryString[ConstantsValues.ChannelId]))
+return;
+var channelId = this.GetChannelId();
+if (!Sitecore.Data.ID.IsNullOrEmpty(channelId))
+{
+args.ChannelId = channelId;
+}
+}
+catch (Exception exception)
+{
+Sitecore.Diagnostics.Log.Error(“Error setting custom channel id for traffic types”, exception,
+typeof(QueryStringChannelResolver));
+}
+}
+public ID GetChannelId()
+{
+var queryString = Sitecore.Context.Request?.QueryString[“ChannelId”];
+ID channelId;
+if (ID.TryParse(queryString, out channelId))
+{
+return channelId;
+}
+return null;
+}
+}
